6986dcf5b32c990e6cb7ad0adc0ea9311d4464af
Larson Collier <collierlarson@gmail.com>
2023-06-13 13:55:21 -0400

    fixed nav styling

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
 
   return (
 
-    <div>
+    <div className="App">
       <BrowserRouter>
         <NavBar />
         <Routes>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,10 +8,12 @@ const NavBar = () => {
     return (
         <div className="navBar">
             <h3 onClick={()=>navigate("/")} className="siteTitle">SWAN THE GOOSE</h3>
-            <h3 onClick={() => navigate("/websites")}>Websites</h3>
-            <h3 onClick={()=>navigate("/art")}>Art</h3>
-            <h3 onClick={() => navigate("/swantogoose")}>The Swan</h3>
-            <h3 onClick={()=>navigate("/bio")}>Bio</h3>
+            <div className="rightNav">
+                <h3 onClick={() => navigate("/websites")}>Websites</h3>
+                <h3 onClick={()=>navigate("/art")}>Art</h3>
+                <h3 onClick={() => navigate("/swantogoose")}>The Swan</h3>
+                <h3 onClick={()=>navigate("/bio")}>Bio</h3>
+            </div>
         </div>
     )
 }
