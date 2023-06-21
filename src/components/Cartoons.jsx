070d3288f7f06ad2016b24ec6eeedcc60af5c345
Larson Collier <collierlarson@gmail.com>
2023-06-21 17:55:33 -0400

    attempted automated file path array

--- src/components/Cartoons.jsx
+++ src/components/Cartoons.jsx
@@ -1,7 +1,40 @@
-const Cartoons = () => {
+import { useEffect, useState } from 'react';
+// import generatePaths from './generatePaths';
+
+// const fs = require('fs');
+const path = require('node:path');
+
+const cartoonsPath = path.join(__dirname, 'public/cartoons');
+
+const generatePaths = () => {
+    const files = fs.readdirSync(cartoonsPath);
+    const paths = files.map((file) => `/${cartoonsPath}/${file}`);
+    return paths;
+};
+
+// module.exports = generatePaths;
+
+function Cartoons() {
+    const [cartoonPaths, setCartoonPaths] = useState([]);
+
+    useEffect(() => {
+        const fetchCartoonPaths = async () => {
+            try {
+                const paths = generatePaths();
+                setCartoonPaths(paths);
+            } catch (error) {
+                console.error('Error generating cartoon paths:', error);
+            }
+        };
+
+        fetchCartoonPaths();
+    }, []);
 
     return (
-        <>Cartoons</>
-    )
+        <div>
+            <img src={cartoonPaths[0]} />
+        </div>
+    );
 }
-export default Cartoons
+
+export default Cartoons;
--- src/components/Sketches.jsx
+++ src/components/Sketches.jsx
@@ -1,7 +1,31 @@
+import { readdirSync } from 'fs';
+import { useEffect, useState } from 'react';
+
+
 const Sketches = () => {
+    const [cartoonPaths, setCartoonPaths] = useState([]);
+
+    useEffect(() => {
+        const fetchCartoonPaths = async () => {
+            try {
+                const directoryPath = 'public/cartoons';
+                const files = readdirSync(directoryPath);
+                const paths = files.map((file) => `/${directoryPath}/${file}`);
+                setCartoonPaths(paths);
+            } catch (error) {
+                console.error('Error reading directory:', error);
+            }
+        };
+
+        fetchCartoonPaths();
+    }, []);
 
     return (
-        <>Sketches</>
-    )
+        <div>
+            {cartoonPaths.map((cartoon)=>{
+                <img src={cartoon} />
+            })}
+        </div>
+    );
 }
 export default Sketches
