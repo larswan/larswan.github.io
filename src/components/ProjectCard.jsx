607960510a32ee55ac5f3ca6ac41764102c96217
Larson Collier <collierlarson@gmail.com>
2023-06-22 14:38:28 -0400

    added project card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,10 +1,26 @@
 import LoomPlayer from "./LoomPlayer"
+import { Card, Space } from 'antd';
+
 
 const ProjectCard = ({link, name, description, techs}) => {
 
     return(
         <div>
-            <LoomPlayer name={name} link={link}/>
+            <Space direction="vertical" size={16}>
+                <Card
+                    title={name}
+                    extra={<a href={link}>Demo</a>}
+                    style={{
+                        width: 300,
+                    }}
+                >
+                    <LoomPlayer name={name} link={link}/>
+                    <p>Card content</p>
+                    <p>Card content</p>
+                    <p>Card content</p>
+                </Card>
+            </Space>
+
         </div>
     )
 }
--- src/components/Sketches.jsx
+++ src/components/Sketches.jsx
@@ -1,8 +1,11 @@
 import { Image } from "antd";
 import { useEffect, useState } from "react";
+import { Button, Space } from 'antd';
 
 const Sketches = () => {
     const [sketchList, setSketchList] = useState([])
+    const [count, setCount] = useState()
+    let max
 
     useEffect(() => {
         const fetchData = async () => {
@@ -10,22 +13,59 @@ const Sketches = () => {
                 const response = await fetch('fileList.json');
                 const data = await response.json();
                 setSketchList(data.sketchList);
-                console.log(data.sketchList)
+                setCount(data.sketchList.length-1)
+                max = data.sketchList.length
+
+                console.log("max "+ max)
+                console.log( data.sketchList)
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
         };
-
         fetchData();
     }, []);
 
+    useEffect(()=>{
+        console.log("count = " + count)
+        ,[count]})
+
+    const increment = () => {
+        console.log(count)
+
+        if (count < max){
+            setCount(0)
+        }
+        else {
+            setCount((prev)=>++prev)
+        }
+    }
+    
+    const decrement = () => {
+        console.log(count)
+        if (count === 0) {
+            setCount(max)
+        }
+        else {
+            setCount((prev)=>--prev)
+        }
+    }
+
+
     return (
         <div>
-            {sketchList.map((sketch, i)=>{
+            <Button type="primary" onClick={()=>decrement()}> back </Button>
+            <Button type="primary" onClick={()=>increment()}>forward</Button>
+           
+            <Image src={sketchList[count]} key={sketchList[count]} />
+           
+            {/* {sketchList.map((sketch, i)=>{
                 return(
                     <Image src={sketch}  key={i} />
-                )
-            })}
+                    )
+                })} */}
+            <div>
+            
+            </div>    
         </div>
     );
 }
