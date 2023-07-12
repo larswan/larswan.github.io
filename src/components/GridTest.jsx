bd20289559a1c94a3284b12c9e9872219926e1e4
Larson Collier <collierlarson@gmail.com>
2023-07-12 17:24:53 -0400

    homePage layout

--- src/components/GridTest.jsx
+++ src/components/GridTest.jsx
@@ -6,33 +6,47 @@ const GridTest = () => {
 
     return(
         <div className="gridContainer">
-            <div onClick={()=>navigate("about")} className="box" style={{ '--background-color': 'blue', backgroundColor: 'var(--background-color)' }}>
+            <div onClick={() => navigate("about")} 
+                className="box" 
+            style={{ '--background-color': '#181307', backgroundColor: 'var(--background-color)' }}>
                 <div className="homeAboutContainer">
                     <h1>ABOUT</h1>
+                    <img id="aboutProfPic" src="public/siteImages/headshot.jpg" />
                     <p>Hi, I'm Larson Collier a front-end developer based in New York. This site is my portfolio.</p>
                 </div>
             </div>
             
-            <div onClick={() => navigate("about")} className="box" style={{ '--background-color': 'white', backgroundColor: 'var(--background-color)' }}>
+            <div onClick={() => navigate("about")} 
+                className="box" 
+                style={{ '--background-color': 'white', backgroundColor: 'var(--background-color)' }}>
                 <h1>Projects</h1>
                 {/* <img src={graphicPaths[0]}/> */}
             </div>
             
-            <div onClick={()=>navigate("cartoons")} className="box" style={{ '--background-color': 'grey', backgroundColor: 'var(--background-color)' }}>
+            <div onClick={()=>navigate("cartoons")} 
+                className="box" 
+                style={{ '--background-color': 'grey', backgroundColor: 'var(--background-color)' }}>
                 <h1>Art</h1>
+                <img src="https://github.com/larswan/larswan.github.io/blob/main/public/cartoons/REPTILES%20NEW%20BOX.png?raw=true" />
             </div>
             
-            <div onClick={()=>navigate("animations")} className="box" style={{ '--background-color': 'teal', backgroundColor: 'var(--background-color)' }}>
+            <div onClick={()=>navigate("animations")} 
+                className="box" 
+                style={{ '--background-color': 'teal', backgroundColor: 'var(--background-color)' }}>
                 <h1>Animations</h1>
                 <img src="https://github.com/larswan/larswan.github.io/blob/main/public/animations/headbounce%20cropped%20gif.gif?raw=true" />
             </div>
-            <div onClick={()=>navigate("graphics")} className="box" style={{ '--background-color': 'green', backgroundColor: 'var(--background-color)' }}>
+            <div onClick={()=>navigate("graphics")} 
+                className="box" 
+                style={{ '--background-color': 'green', backgroundColor: 'var(--background-color)' }}>
                 <h1>Graphics</h1>
+                <img src="https://github.com/larswan/larswan.github.io/blob/main/public/graphics/pure%20genius%204.18.22%20final%20B.png?raw=true" />
             </div>
             
-            <div className="box" style={{ '--background-color': 'pink', backgroundColor: 'var(--background-color)' }}>
+            {/* <div className="box" 
+                style={{ '--background-color': 'grey', backgroundColor: 'var(--background-color)' }}>
                 <h1>SOCIALS</h1>
-            </div>
+            </div> */}
         </div>
     )
 }
