f82c8cb1504d6232e59e5526bdd1faf09a30bc3b
Larson Collier <collierlarson@gmail.com>
2023-07-19 18:01:26 -0400

    vertical layout achieved, sizes reworked. going to seperate a header box for title and contact

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,31 +11,31 @@ const About = () => {
     return (
         <div className="pageContainer">
             <div className="about">
+                
+                <img className="aboutProfPic" src="public/siteImages/headshot.jpg"/>
+                <h3>Larson Collier</h3>
+                <div className="contactIcons">
+                    <LinkedinFilled />
+                    <GithubFilled />
+                    <MailFilled />
+                </div>
 
-                <div className="leftAbout">
-                    <img id="aboutProfPic" src="public/siteImages/headshot.jpg"/>
-                    <h3>Larson Collier</h3>
-                    <div className="contactIcons">
-                        <LinkedinFilled />
-                        <GithubFilled />
-                        <MailFilled />
-                    </div>
-                </div>
-                <div className="rightAbout">
-                    <p>
-                        Hi, I’m Larson a software develper born adn raised in Atlatna, Ga now living in New York City. Throughtout my life I’ve enjoyed drawing and playing music.
-                        <br></br><br></br>
-                        In my time at the University of Georgia in Athens I played in a vareity of bands, write and recording music and releasing it under the banner of Goe Bidem Records (link this to spotify playlist). At the time Joe Biden was a goofy former Vice President so it came off different. I also began creating cartoons and animations under the pseudonym Blanko. During this time I also designed and prototyped an accesory for guitar players called Pickit that won the 2016 UGA Idea Accelerator Program.
-                        <br></br><br></br>
-                        Following college I worked a variety of positions in the film and music industries from snake wrangler to audio engineer and art director. I continued creating music, cartoons and animatons as well as doing freelance video-editing and design work.
-                        <br></br><br></br>
-                        After moving to New York I decided to make a career change______ . My goal was to find a path that involved creativity and complex problem solving, and when a friend told me about his work as a software engineer I saw it as a perfect match.
-                        <br></br><br></br>
-                        Ideation gives me loads of energy, as does creative collaboritaion, and software engineering  provides the perfect vehicle. I’m excited to be part of a constantly evolving discipline that provides so many opprotunities to build and innovate. 
-                    </p>
-                </div>
+                <p>
+                    Hi, I’m Larson a software develper born adn raised in Atlatna, Ga now living in New York City. Throughtout my life I’ve enjoyed drawing and playing music.
+                    <br></br><br></br>
+                    In my time at the University of Georgia in Athens I played in a vareity of bands, write and recording music and releasing it under the banner of Goe Bidem Records (link this to spotify playlist). At the time Joe Biden was a goofy former Vice President so it came off different. I also began creating cartoons and animations under the pseudonym Blanko. During this time I also designed and prototyped an accesory for guitar players called Pickit that won the 2016 UGA Idea Accelerator Program.
+                    <br></br><br></br>
+                    Following college I worked a variety of positions in the film and music industries from snake wrangler to audio engineer and art director. I continued creating music, cartoons and animatons as well as doing freelance video-editing and design work.
+                    <br></br><br></br>
+                    After moving to New York I decided to make a career change______ . My goal was to find a path that involved creativity and complex problem solving, and when a friend told me about his work as a software engineer I saw it as a perfect match.
+                    <br></br><br></br>
+                    Ideation gives me loads of energy, as does creative collaboritaion, and software engineering  provides the perfect vehicle. I’m excited to be part of a constantly evolving discipline that provides so many opprotunities to build and innovate. 
+                </p>
+            
                 <Tabs defaultActiveKey="1"
                     type="card"
+                    // fontSize="10"
+                    className="tabs"
                     items={
                         [
                             {label: (
