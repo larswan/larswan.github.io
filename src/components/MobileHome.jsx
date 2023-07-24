34c809781c7473d10c5340ef20a8d098fd3a7625
Larson Collier <collierlarson@gmail.com>
2023-07-23 21:17:20 -0400

    added scroll functions

--- src/components/MobileHome.jsx
+++ src/components/MobileHome.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import { CaretRightOutlined, CaretDownOutlined } from '@ant-design/icons';
+import { CaretRightOutlined, CaretDownOutlined, CaretUpOutlined } from '@ant-design/icons';
 
 const MobileHome = () => {
     const navigate = useNavigate()
@@ -9,9 +9,20 @@ const MobileHome = () => {
         "#D5D3CC",
     ]
 
+    function smoothScroll (id) {
+            id.scrollIntoView({ behavior: "smooth" });
+        }
+
+    function scrollToTop() {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    }
+
     return (
         <div className="mobileHomeContainer">
-            <div className="mobileHomeBox">
+            <div id="box1" className="mobileHomeBox">
                 <div className="mobileHomeBlock">
                     <img onClick={() => navigate("about")} className="mobileHomeProfPic" src="https://github.com/larswan/larswan.github.io/blob/main/public/siteImages/headshot.jpg?raw=true" />
                     <h1>Larson Collier</h1>
@@ -21,24 +32,34 @@ const MobileHome = () => {
                             Hi, I'm Larson, a web developer based in New York City.<br></br><br></br> You can find demos of applications I've built as well as examples of my art, design, and animation here. You can contact my through the links provided on the about page.
                         </p>
                     </div>
-                    <CaretDownOutlined className="scrolldownDownArrow"/>
+                    <a onClick={()=>smoothScroll(box2)}>
+                        <CaretDownOutlined className="scrolldownDownArrow"/>
+                    </a>
                 </div>
             </div>
-            <div onClick={() => navigate("projects")} className="mobileHomeBox">
+            <div id="box2" className="mobileHomeBox">
                 <div className="mobileHomeBlock">
                     <img className="mobileHomePageDemo" src="https://github.com/larswan/larswan.github.io/blob/main/public/siteImages/guessdamateCompressedGif.gif?raw=true" />
                     <h1><small><em>Featured Project:</em></small> GuessDaMate</h1>
-                    <a href="https://www.loom.com/embed/f6f3ce52544f457da8ba9673a6336c6f?sid=231f2dcd-1663-4260-93b8-bf2df82c4b98"><h2>Watch Demo</h2></a>
+                    <a href="https://www.loom.com/embed/f6f3ce52544f457da8ba9673a6336c6f?sid=231f2dcd-1663-4260-93b8-bf2df82c4b98"><h2>Watch Full Demo</h2></a>
+                    <p>
+                        GuessDaMate, web based take on the classic board game 'Guess Who?', allows users to play against friends using their Google account in a mobile first designed interface.'
+                    </p>
+                    <a onClick={() => smoothScroll(box3)}>
+                        <CaretDownOutlined className="scrolldownDownArrow" />
+                    </a>
                 </div>
             </div>
-            <div onClick={() => navigate("about")} className="mobileHomeBox">
+            <div id="box3" className="mobileHomeBox">
                 <div className="mobileHomeBlock">
 
                 </div>
             </div>
-            <div onClick={() => navigate("about")} className="mobileHomeBox">
+            <div className="mobileHomeBox">
                 <div className="mobileHomeBlock">
-
+                    <a onClick={() => scrollToTop()} >
+                        <CaretUpOutlined />
+                    </a>
                 </div>
             </div>
 
