7dac3fbb92a287397805b928d105a7a6be037524
Larson Collier <collierlarson@gmail.com>
2023-07-25 22:21:03 -0400

    updated desktopHome

--- src/components/DesktopHome.jsx
+++ src/components/DesktopHome.jsx
@@ -40,34 +40,32 @@ const DesktopHome = () => {
 
                         <div className="homeAboutText">
                             <p>
-                                I'm Larson Collier, a developer based in New York. 
-                                <br></br><br></br>
-                                You can find examples of my previous applications as well as my art animations and design work. design. I strive to incorporate my background in art, animation, and design into the applications I build to create engaging and fun sites.
-                                <br></br><br></br>
-                                Currently I'm building a web based application that takes in an artist name and uses goolg api to source interviews from the artsit. These docuemtns are then passed to an LLM to generate a list of recommendations fbased on the artsits inspirations
-
-                                
+                                Hi, I'm Larson, a web developer based in New York City.<br></br><br></br>
+                                You can find demos of <a onClick={() => navigate("projects")} target="_blank" rel="noopener noreferrer" style={{ color: 'inherit' }}><b>applications</b></a> I've built as well as examples of my <a onClick={() => navigate("cartoons")} target="_blank" rel="noopener noreferrer" style={{ color: 'inherit' }}><b>art</b></a>, <a onClick={() => navigate("design")} target="_blank" rel="noopener noreferrer" style={{ color: 'inherit' }}><b>designs</b></a>, and <a onClick={() => navigate("animations")} target="_blank" rel="noopener noreferrer" style={{ color: 'inherit' }}><b>animations</b></a> here.<br></br><br></br>
+                                Feel free to reach out through the links provided on the <a onClick={() => navigate("about")} target="_blank" rel="noopener noreferrer" style={{ color: 'inherit' }}><b>about</b></a> page!
                             </p>
                         </div>
                     </div>
                 </div>
             </div>
             
-            <div onClick={() => navigate("projects")} 
+            <div  
                 className="box" 
                 style={{ '--background-color': 'white', backgroundColor: 'var(--background-color)' }}>
                 
-                <div className="boxTitleContainer">
-                    <h1 className="homePageProjectsTitle">Featured Project</h1>
+                <div className="boxTitleContainer" onClick={() => navigate("projects")}>
+                    <h1 className="homePageProjectsTitle">Projects</h1>
                     <CaretRightOutlined className="homeArrowIcon" />
                 </div>
                 <div className="homePageProjectContents">    
                     <img className="homePageDemo" src="https://github.com/larswan/larswan.github.io/blob/main/public/siteImages/guessdamateCompressedGif.gif?raw=true" />
                     <div className="homeProjectDesciption">
-                        <h3>GuessDaMate</h3>
+                        <h3>Featured Project: GuessDaMate</h3>
+                        <a href="https://www.loom.com/embed/f6f3ce52544f457da8ba9673a6336c6f?sid=231f2dcd-1663-4260-93b8-bf2df82c4b98"><h2 id="desktopDemoButton">Watch Demo</h2></a>
                         <p>
-                            I thoroughly enjoyed the process of building the project "GuessDamate" from scratch, and I'd like to share my experience in the first person. My goal was to create an online version of the classic game Guess Who? that enables users to play with their friends using various sets of cards. To accomplish this, I harnessed the power of React JS to design and develop a user-friendly interface. With each phase of the game carefully considered, I ensured that players could navigate seamlessly through the entire experience. <br></br><br></br>
-                            One of the key elements in building "GuessDamate" was implementing the Design Thinking methodology. By adopting this approach, I was able to focus on the user's needs and preferences, resulting in an intuitive UI and an immersive UX design strategy. The game became a delightful experience as players effortlessly interacted with the application, thanks to the thoughtful user interface. Additionally, I utilized a combination of useEffect and a switch statement to create a dynamic routing system based on fetched data. This allowed the game to smoothly transition between different game phases, ensuring a seamless and engaging experience for players throughout their GuessDamate journey. 
+                            GuessDaMate is my web-based take on the classic board game Guess Who?<br></br><br></br>
+                            Users sign in and add friends using their Google accounts. They then select a set of famous people and take turns trying to guess the other player's card.<br></br><br></br>
+                            This project helped me strengthen my skills in mobile-first design as well as handling game logic.
                         </p>
                     </div>
                 </div>
