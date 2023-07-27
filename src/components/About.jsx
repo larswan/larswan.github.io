4c690c1e84d107fb0af03f0e81a0d5f02caa4610
Larson Collier <collierlarson@gmail.com>
2023-07-27 16:35:47 -0400

    cursor pointer on h1 hover. Updated about bio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -33,15 +33,14 @@ const About = () => {
                 </div>
                 <hr></hr>
                 <p>
-                    Hi, I'm a software developer born and raised in Atlanta, GA now living in New York City. Throughout my life I’ve enjoyed drawing, playing music, and inventing little things.
+                    Hi, I'm a software developer born and raised in Atlanta, GA, now living in New York City. Throughout my life I’ve enjoyed drawing, playing music, and inventing little things.
                     <br></br><br></br>
                     In my time at the University of Georgia in Athens I played in a variety of bands, writing and recording music and releasing it under the banner of <a href="https://open.spotify.com/artist/087EadRmsB55nFpxzgHljT?si=bl9vKIhtS8GRWRu4imx37g" target="_blank" rel="noopener noreferrer" style={{ color: 'inherit' }}><b>Goe Bidem Records</b></a>. I began creating cartoons, animations, and music under the pseudonym <a href="https://open.spotify.com/artist/087EadRmsB55nFpxzgHljT?si=bl9vKIhtS8GRWRu4imx37g" target="_blank" rel="noopener noreferrer" style={{ color: 'inherit' }}><b>Blanko</b></a> and my music video "<a href="https://www.youtube.com/watch?v=ktLZu3rV88I" target="_blank" rel="noopener noreferrer" style={{ color: 'inherit' }}><b>Hallelujah</b></a>" was selected for the Indie Grits Film Festival. During this time I also designed and prototyped an accessory for guitar players called Pickit that won the 2016 UGA Idea Accelerator Program. 
                     <br></br><br></br>
-                    Following college I worked a variety of positions in the film and music industries from snake wrangler to audio engineer and art director. I continued creating music, cartoons and animations as well as doing freelance video-editing and design work.
+                    Following college I worked a variety of positions in the film and music industries from snake wrangler to audio engineer and art director, as well as doing freelance video-editing and design work.<br></br><br></br>
+                    After moving to New York I decided to make a career change. My goal was to find a path that involved creativity and complex problem solving; so when a friend told me about his work as a software engineer I recognized it as a career where I could leverage the skills in ideation and collaboration that I'd developed.
                     <br></br><br></br>
-                    After moving to New York I decided to make a career change______ . My goal was to find a path that involved creativity and complex problem solving, and when a friend told me about his work as a software engineer I saw it as a perfect match.
-                    <br></br><br></br>
-                    Ideation gives me loads of energy, as does creative collaboration, and software engineering  provides the perfect vehicle. I’m excited to be part of a constantly evolving discipline that provides so many opportunities to build and innovate. 
+                    Problem solving gives me loads of energy, as does creative collaboration, and software engineering provides the perfect combination of these elements of work. I’m excited to be part of a constantly evolving discipline that provides so many opportunities to build and innovate. 
                 </p>
                 <Collapse 
                 className="aboutCollapse"
