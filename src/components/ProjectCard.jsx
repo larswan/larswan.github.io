d479c501b38e9fc7bcb250e01011340b33c6154f
Larson Collier <collierlarson@gmail.com>
2023-08-01 03:55:03 -0400

    updated project descriptions and links

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import LoomPlayer from "./LoomPlayer"
 import { Card, Space } from 'antd';
 
-const ProjectCard = ({ link, name, description, tech }) => {
+const ProjectCard = ({ link, loom, linkTitle, name, description, tech }) => {
     const [width, setWidth] = useState(window.innerWidth);
 
     useEffect(() => {
@@ -21,18 +21,20 @@ const ProjectCard = ({ link, name, description, tech }) => {
 
     return (
         <Card
-            cover={<LoomPlayer name={name} link={link} />}
+            cover={<LoomPlayer name={name} link={loom} />}
             title={<b>{name}</b>}
             extra={<a href={link} style={{ backgroundColor: '#1677ff', color: 'white', padding: '5px 10px', borderRadius: '10px' }}>
-                <b>Demo</b>
+                <b>{linkTitle}</b>
             </a>}
             style={{
                 width: isMobile ? 350 : 800
             }}
             key={name}
         >
-            <p>{description}</p>
-            <p><b>{tech}</b></p>
+            <div style={{display: "flex", flexDirection: "column"}}>
+                <p style={{margin: 0}}>{description}</p>
+                <p style={{marginBottom: 0}}><b>Key Technologies: {tech}</b></p>
+            </div>
         </Card>
     );
 };
--- src/components/Websites.jsx
+++ src/components/Websites.jsx
@@ -4,20 +4,26 @@ import ProjectCard from './ProjectCard'
 const projects = [
     {
         name: "GuessDaMate",
+        linkTitle: "Watch Demo",
+        loom: "https://www.loom.com/embed/f6f3ce52544f457da8ba9673a6336c6f?sid=231f2dcd-1663-4260-93b8-bf2df82c4b98",
         link: "https://www.loom.com/embed/f6f3ce52544f457da8ba9673a6336c6f?sid=231f2dcd-1663-4260-93b8-bf2df82c4b98",
-        description: "Online version of the classic game Guess Who? allowing users to play with their friends using different sets of cards.",
+        description: "GuessDaMate is my web-based take on the classic board game Guess Who? Users sign in and add friends using their Google accounts. They then select a set of famous people and take turns trying to guess the other player's card.\nThis project helped strengthen my skills in mobile-first design as well as handling game logic. One main challenge was designing the database with expansibility in mind.\nI chose PostgreSQL so that I could pass complex 'turn' objects with values for question, answer, turnNumber, flippedCards array, playerID, and other useful data. This structure allows for additional features such as 'turn history', or for the losing player to see at the end of the game which question made them falsely turn over the correct card.",
         tech: "React, Rails, GAPI, PostgreSQL"
     },
     {
         name: "DriveTunes",
+        linkTitle: "Watch Demo",
+        loom: "https://www.loom.com/embed/218323e656be417195be4648a21746f1?sid=4ee451ba-bf52-49e7-968e-e0fc101682fd",
         link: "https://www.loom.com/embed/218323e656be417195be4648a21746f1?sid=4ee451ba-bf52-49e7-968e-e0fc101682fd",
-        description: "Browser based library for listening to music files stored on personal Google Drive accounts in a more traditional music library format.",
+        description: "DriveTunes is a browser based music library for files stored on personal Google Drive accounts. The idea was based on a personal need, as I store and listen to my demos and other songs not on Spotify with Google Drive, which only displays as a file browser and does not store metadata such as song title, album, album art, artist, track number etc. I built the front-end while partner Vincent Le built the backend.\nUsers login with Google OAuth and authorize access to their files in their drive.When they click ‘Import Files’ DriveTunes scans for any music files that are not already in their library and allows users to select songs to add to their library.On the ‘Manage Files’ screen users can update metadata so that their songs are properly labeled and grouped.\nI enjoyed familiarizing myself with GAPI as well as synthesizing elements of iTunes, SoundCloud, Spotify, and Google UIs to design the interface.",
         tech: "React, Rails, GAPI"
     },
     {
         name: "Classpass Clone",
-        link: "https://www.loom.com/embed/7343196950cc4503bfcce165dcc88e66?sid=92733bcc-5aa9-421d-8cba-5739aefee203",
-        description: "Clone of the workout service Classpass created as an excercise in web layout and CSS using minimal pre-built components. ",
+        loom: "https://www.loom.com/embed/7343196950cc4503bfcce165dcc88e66?sid=92733bcc-5aa9-421d-8cba-5739aefee203",
+        link: "https://classpass.com/studios/yogasole-brooklyn-wsle",
+        linkTitle: "View Reference",
+        description: "In this project I built a front-end clone of the fitness service ClassPass using Tailwind CSS and minimal pre-built components. This was largely an exercise in reverse engineering CSS to practice meeting specific design requirements. Click 'View Reference' for comparison.",
         tech: "React, Tailwind, CSS, Google OAuth"
     },
 ]
@@ -26,15 +32,25 @@ const projects = [
 const Websites = ()=> {
 
     return (
-            <div className="projectsContainer">
-            {
-                projects.map((project, index)=>{
-                    return(
-                        <ProjectCard name={project.name} link={project.link} description={project.description} tech={project.tech}  key={index}/>
-                        )
-                    })
-                }
-            </div>
+        <div className="projectsContainer">
+            {projects.map((project, index) => {
+                const descriptionWithLineBreaks = project.description.split('\n').map((part, idx) => (
+                    <p key={idx}>{part}</p>
+                ));
+
+                return (
+                    <ProjectCard
+                        name={project.name}
+                        loom={project.loom}
+                        linkTitle={project.linkTitle}
+                        link={project.link}
+                        description={descriptionWithLineBreaks}
+                        tech={project.tech}
+                        key={index}
+                    />
+                );
+            })}
+        </div>
     )
 }
 export default Websites
